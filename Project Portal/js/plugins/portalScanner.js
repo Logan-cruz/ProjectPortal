5a6c3ea9fb80412e8e4a6e6f62121f070dae13d0
luke24279 <82524389+luke24279@users.noreply.github.com>
2023-02-27 14:59:15 -0500

    Refined picking characters

--- Project Portal/js/plugins/portalScanner.js
+++ Project Portal/js/plugins/portalScanner.js
@@ -8,19 +8,25 @@ Game_Interpreter.prototype.pluginCommand = function (command, args) {
     _Game_Interpreter_pluginCommand.call(this, command, args);
     if (command === 'scan') {
         var newChar = prompt("Which character do you want to play as now?")
+        while (newChar != "StealthElf" && newChar != "LittleMac" && newChar != "Benny") {
+            newChar = prompt("Please input a valid character.")
+        }
+        console.log(newChar);
+        $gameParty.removeActor(charNum)
         switch (newChar) {
             case "StealthElf":
-                var charNum = 1
-                break;
-            case "LittleMac":
                 var charNum = 2
                 break;
-            case "Benny":
+            case "LittleMac":
                 var charNum = 3
+                break;
+            case "Benny":
+                var charNum = 4
                 break;
             default:
                 break;
         }
         $gameVariables.setValue(1, charNum);
+        console.log($gameVariables.value(1));
     }
 }
